58662873faa0bb3cfa3b858bb665b85524cdc383
galacado <g.cado@student.vu.nl>
2024-09-30 21:00:05 +0200

    learning inheritance

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -14,7 +14,7 @@ contract SimpleStorage {
 
     mapping(string => uint256) public nameToFavoriteNumber;
 
-    function store(uint256 _favouriteNumber) public {
+    function store(uint256 _favouriteNumber) public virtual {
         favouriteNumber = _favouriteNumber;
     }
 
