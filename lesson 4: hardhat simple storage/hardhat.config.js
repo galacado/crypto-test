9b90c9fc2832f8b03befc5c523e067756c47c3a2
Gala Cado <97437130+galacado@users.noreply.github.com>
2024-10-01 22:01:51 +0200

    hardhat testing

--- lesson 4: hardhat simple storage/hardhat.config.js
+++ lesson 4: hardhat simple storage/hardhat.config.js
@@ -1,6 +1,37 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("hardhat-gas-reporter")
+require("dotenv").config()
+require("./tasks/block-number")
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/your-api-key"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+
 module.exports = {
-  solidity: "0.8.27",
+    networks: {
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+        },
+        // yarn hardhat node:
+        localhost: {
+            url: "http://localhost:8545",
+            chainId: 31337,
+        },
+    },
+    solidity: "0.8.27",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        gasPrice: 20,
+    },
 };
--- lesson 4: hardhat simple storage/scripts/deploy.js
+++ lesson 4: hardhat simple storage/scripts/deploy.js
@@ -1,12 +1,49 @@
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 async function main() {
+    // DEPLOY CONTRACT:
     const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
     console.log("Deploying contract...")
     const simpleStorage = await SimpleStorageFactory.deploy()
     await simpleStorage.waitForDeployment()
 
     console.log(`Deployed contract to: ${simpleStorage.target}`)
+
+    // VERIFY ON ETHERSCAN:
+    if(network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY){
+        console.log("Waiting for block txes...")
+        await simpleStorage.deploymentTransaction().wait(6)
+        await verify(simpleStorage.target, [])
+    }
+
+    // INTERACT WITH CONTRACT:
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current Value is: ${currentValue}`)
+
+    // Update the current value
+    const transactionResponse = await simpleStorage.store(7)
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated Value is: ${updatedValue}`)
+}
+
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...")
+    try{
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } 
+    catch (e) {
+        if (e.message.toLowerCase().includes("already verified")){
+            console.log("Already verified!")
+        }
+        else{
+            console.log(e)
+        }
+    }
+
 }
 
 main()
