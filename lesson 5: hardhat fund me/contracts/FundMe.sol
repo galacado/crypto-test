e43d83592fce8288edadad5b3e2ddadeb709e00b
Gala Cado <97437130+galacado@users.noreply.github.com>
2024-10-03 15:32:58 +0200

    update hardhat deployment of fund me

--- lesson 5: hardhat fund me/contracts/FundMe.sol
+++ lesson 5: hardhat fund me/contracts/FundMe.sol
@@ -1,67 +1,57 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
-// 1 ether = 1e18
-
-error NotOwner();
+error FundMe__NotOwner();
 
 contract FundMe {
     using PriceConverter for uint256;
 
     uint256 public constant MINIMUM_USD = 50 * 1e18;
-
-    address[] public funders;
-    mapping(address => uint256) public addressToAmountFunded;
-
     address public immutable i_owner;
-
-    constructor(){
-        i_owner = msg.sender;
-    }
-
-    function fund() public payable{
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough ether :("); 
-
-        for(uint256 i = 0; i < funders.length; i++){
-            if(funders[i] == msg.sender){
-                addressToAmountFunded[msg.sender] += msg.value;
-            }
-            else{
-                funders.push(msg.sender);
-                addressToAmountFunded[msg.sender] = msg.value;
-            }
-        }
-    }
-
-    function withdraw() public onlyOwner{
-        for(uint256 i = 0; i < funders.length; i++){
-            address funder = funders[i];
-            addressToAmountFunded[funder] = 0;
-        }
-
-        funders = new address[](0);
-
-        // transfer:
-        // payable(msg.sender).transfer(address(this).balance);
-        
-        // send:
-        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        // require(sendSuccess, "Send failed :(");
-
-        // call: recommended
-        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
-        require(callSuccess, "Call failed :(");
-    }
+    address[] public s_funders;
+    mapping(address => uint256) public s_addressToAmountFunded;
+    AggregatorV3Interface public s_priceFeed;
 
     modifier onlyOwner {
-        // require(msg.sender == i_owner, "Sender is not owner!!");
-        if(msg.sender != i_owner) { revert NotOwner(); }
+        if(msg.sender != i_owner) { revert FundMe__NotOwner(); }
         _;
+    }
+    
+    constructor(address priceFeedAddress){
+        i_owner = msg.sender;
+        s_priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
     receive() external payable { fund(); }
 
     fallback() external payable { fund(); }
+
+    function fund() public payable{
+        require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "Didn't send enough ether :("); 
+
+        for(uint256 i = 0; i < s_funders.length; i++){
+            if(s_funders[i] == msg.sender){
+                s_addressToAmountFunded[msg.sender] += msg.value;
+            }
+            else{
+                s_funders.push(msg.sender);
+                s_addressToAmountFunded[msg.sender] = msg.value;
+            }
+        }
+    }
+
+    function withdraw() public onlyOwner{
+        for(uint256 i = 0; i < s_funders.length; i++){
+            address funder = s_funders[i];
+            s_addressToAmountFunded[funder] = 0;
+        }
+
+        s_funders = new address[](0);
+
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed :(");
+    }
 }
